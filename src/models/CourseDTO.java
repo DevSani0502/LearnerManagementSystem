85b7c0312f83a38369e24cbe9c95b6bbea341b13
Sani0502 <devsanitiwari@gmail.com>
2025-12-30 22:31:32 +0530

    All changes

--- src/models/CourseDTO.java
+++ src/models/CourseDTO.java
@@ -1,4 +1,58 @@
 package models;
 
 public class CourseDTO{
+
+    private String id;
+    private String courseName;
+    private String description;
+    private String durationInWeeks;
+    private boolean active;
+
+    public CourseDTO(String id, String courseName, String description, String durationInWeeks, boolean active) {
+        this.id = id;
+        this.courseName = courseName;
+        this.description = description;
+        this.durationInWeeks = durationInWeeks;
+        this.active = active;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDurationInWeeks() {
+        return durationInWeeks;
+    }
+
+    public void setDurationInWeeks(String durationInWeeks) {
+        this.durationInWeeks = durationInWeeks;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
 }
--- src/models/EnrollmentDTO.java
+++ src/models/EnrollmentDTO.java
@@ -1,4 +1,58 @@
 package models;
 
 public class EnrollmentDTO {
+
+    private String id;
+    private String studentId;
+    private String courseId;
+    private String enrollmentDate;
+    private String status;
+
+    public EnrollmentDTO(String id, String studentId, String courseId, String enrollmentDate, String status) {
+        this.id = id;
+        this.studentId = studentId;
+        this.courseId = courseId;
+        this.enrollmentDate = enrollmentDate;
+        this.status = status;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(String courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getEnrollmentDate() {
+        return enrollmentDate;
+    }
+
+    public void setEnrollmentDate(String enrollmentDate) {
+        this.enrollmentDate = enrollmentDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
